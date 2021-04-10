dfbb1918bc1699dc171fc2572335d0bd27c0fcb3
Deivis <deivisutp@gmail.com>
2021-04-10 19:29:40 -0300

    removing redis cache

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,14 +2,14 @@ const { Router } = require("express");
 const ValidationAuth = require("../validations/ValidationAuth");
 const AuthController = require("../controllers/AuthController");
 const authMiddleware = require("../middleware/auth");
-const redisMiddleware = require("../middleware/redis");
-const cache_me = require("../middleware/redisCache");
+//const redisMiddleware = require("../middleware/redis");
+//const cache_me = require("../middleware/redisCache");
 
 const routes = Router();
 
 
 routes.post("/", ValidationAuth.login, AuthController.login);
 
-routes.get("/me", authMiddleware, redisMiddleware, cache_me, AuthController.me);
+//routes.get("/me", authMiddleware, redisMiddleware, cache_me, AuthController.me);
 
 module.exports = routes;
